aaf78b2506a996213e76b9090ffb490f3143d1a3
jsiripun <jed.siripun@digidems.com>
2024-05-23 16:06:26 -0700

    [JS] as is

---
+++ src/app/HelloSection.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Theme } from '@mui/material/styles';
+import { SxProps } from '@mui/system';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Image from 'next/image'
+
+const item: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  px: 5,
+};
+
+function HelloSection() {
+  return (
+    <Box
+      component="section"
+      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'blue' }}
+    >
+      <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
+        <Image src="/hello_surprise.jpg" alt="bg" layout="fill" objectFit='cover' objectPosition='center' />
+
+        {/* <Image src="/hello_surprise.jpg" alt="bg" width="64" height="64" /> */}
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              {/* <Box
+                component="img"
+                src="/static/themes/onepirate/productValues1.svg"
+                alt="suitcase"
+                sx={{ height: 55 }}
+              /> */}
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              {/* <Box
+                component="img"
+                src="/static/themes/onepirate/productValues2.svg"
+                alt="graph"
+                sx={{ height: 55 }}
+              /> */}
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+                rip rip rip
+              {/* <Box
+                component="img"
+                src="/static/themes/onepirate/productValues3.svg"
+                alt="clock"
+                sx={{ height: 55 }}
+              /> */}
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default HelloSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 import Link from 'next/link'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import HelloSection from '@/app/HelloSection'
  
 export const metadata: Metadata = {
   title: 'Jed Siripun',
@@ -9,8 +11,11 @@ export const metadata: Metadata = {
 function HomePage() {
    return (
       <>
-         <div>Jed Siripun</div>
+         {/* <Image src="/hello_surprise.jpg" alt="bg" width="64" height="64" /> */}
+         <HelloSection/>
+         {/* <div>Jed Siripun</div>
          <Link href="/about">First Post</Link>
+         <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1> */}
       </>	    
    )
 }
