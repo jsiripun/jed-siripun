83560960dac8f108e5bcaf1fb6418b2cf99380b2
jsiripun <jed.siripun@digidems.com>
2024-06-06 22:00:21 -0700

    yep

--- src/app/HelloSection.tsx
+++ src/app/HelloSection.tsx
@@ -5,7 +5,7 @@ import { SxProps } from '@mui/system';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import Image from "next/image"
+import Image from "next/legacy/image"
 import { FadeInComponent } from '../components/fadeInComponent'
 import { WaitToRenderComponent } from '@/components/waitToRenderComponent';
 
